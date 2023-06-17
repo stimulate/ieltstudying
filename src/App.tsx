11867081a61a27343557eddc2583e7003fd9c3e7
张高科 <gaoke.zhang@qq.com>
2023-06-17 20:15:33 +0800

    feature:安装路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import MainLayout from './layout';
-import './App.css';
-import {ConfigProvider} from 'antd';
-
-
+import React from 'react'
+import MainLayout from './layout'
+import './App.css'
+import { ConfigProvider } from 'antd'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: '#1677ff',
-      },
-    }}
-  > <MainLayout/>
-  </ConfigProvider>
-  );
+      theme={{
+        token: {
+          colorPrimary: '#1677ff',
+        },
+      }}>
+      {/* <MainLayout/> */}
+      <BrowserRouter>
+        {/* <Routes>
+          <Route path="/" element={MainLayout}></Route>
+        </Routes> */}
+      </BrowserRouter>
+    </ConfigProvider>
+  )
 }
 
-export default App;
+export default App
