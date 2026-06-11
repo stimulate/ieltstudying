27a4768cbc0854ed78290793ada510ae9a5eadc4
Sabrina Yu <stimulate@users.noreply.github.com>
2026-06-11 11:17:09 +0900

    Update 777.ps1

--- public/777.ps1
+++ public/777.ps1
@@ -16,3 +16,18 @@ Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "ScreenSaveTimeOut" -
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "ScreenSaverIsSecure" -Value "1" -Type String
 
 exit 0
+
+$folder = "C:\ProgramData\CompanyScreensaver"
+New-Item -Path $folder -ItemType Directory -Force | Out-Null
+Copy-Item ".\images\*.jpg" -Destination $folder -Force
+
+# 设置 NTFS 权限,禁止普通用户修改/删除
+$acl = Get-Acl $folder
+$acl.SetAccessRuleProtection($true, $false)
+$adminRule = New-Object System.Security.AccessControl.FileSystemAccessRule("BUILTIN\Administrators","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$systemRule = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\SYSTEM","FullControl","ContainerInherit,ObjectInherit","None","Allow")
+$usersRule = New-Object System.Security.AccessControl.FileSystemAccessRule("BUILTIN\Users","ReadAndExecute","ContainerInherit,ObjectInherit","None","Allow")
+$acl.SetAccessRule($adminRule)
+$acl.SetAccessRule($systemRule)
+$acl.SetAccessRule($usersRule)
+Set-Acl -Path $folder -AclObject $acl
